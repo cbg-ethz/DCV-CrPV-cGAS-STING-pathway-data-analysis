158dea972e537d7d89c84587b87d208a29dc0f06
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-08-08 10:00:49 +0200

    runviloca update param

--- workflow/scripts/run_viloca.py
+++ workflow/scripts/run_viloca.py
@@ -4,15 +4,16 @@ from pathlib import Path
 def main(
     fname_bam,
     fname_reference_dcv,
-    fname_reference_crpv,
-    fname_reference_p0_dcv,
-    fname_reference__p0_crpv,
+    #fname_reference_crpv,
+    #fname_reference_p0_dcv,
+    #fname_reference__p0_crpv,
     fname_snv_vcf,
     fname_snv_csv,
     fname_csv,
     dname_work):
 
     # get the correct reference
+    print(fname_bam)
     if "parental" in str(fname_bam):
         if "dcv" in str(fname_bam):
             fname_reference = fname_reference_dcv.resolve()
@@ -62,9 +63,9 @@ if __name__ == "__main__":
     main(
         Path(snakemake.input.fname_bam),
         Path(snakemake.input.fname_reference_dcv),
-        Path(snakemake.input.fname_reference_crpv),
-        Path(snakemake.input.fname_reference_p0_dcv),
-        Path(snakemake.input.fname_reference__p0_crpv),
+        #Path(snakemake.input.fname_reference_crpv),
+        #Path(snakemake.input.fname_reference_p0_dcv),
+        #Path(snakemake.input.fname_reference__p0_crpv),
         Path(snakemake.output.fname_snv_vcf),
         Path(snakemake.output.fname_snv_csv),
         Path(snakemake.output.fname_csv),
