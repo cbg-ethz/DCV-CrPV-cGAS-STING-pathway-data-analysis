51748413524a60567d119ddff5ce7fc7bcc26a1c
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-09-18 17:23:18 +0200

    run crpv workflow

--- run_workflow_crpv.sh
+++ run_workflow_crpv.sh
@@ -3,10 +3,12 @@ sbatch \
   --mail-type=END \
   --mem-per-cpu=2000 \
   --time=120:00:00 \
-  -o snake.out -e snake.err \
+  -o snake_crpv.out -e snake_crpv.err \
 snakemake \
   --profile profile_simple/ \
+  -s workflow/Snakefile_crpv \
   --rerun-incomplete \
+  --keep-incomplete \
   -pr \
   --cores 200 \
   --use-conda \
