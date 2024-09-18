438799d712ddea71cf2720af66cacb80b4cd0ad3
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-09-18 17:25:18 +0200

    update params

---
+++ run_workflow_dcv.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+sbatch \
+  --mail-type=END \
+  --mem-per-cpu=2000 \
+  --time=120:00:00 \
+  -o snake.out -e snake.err \
+snakemake \
+--profile profile_simple/ \
+-s workflow/Snakefile_dcv \
+--rerun-incomplete \
+--keep-incomplete \
+-pr \
+--cores 200 \
+--use-conda \
+--latency-wait 30 \
+--show-failed-logs \
+"$@"
