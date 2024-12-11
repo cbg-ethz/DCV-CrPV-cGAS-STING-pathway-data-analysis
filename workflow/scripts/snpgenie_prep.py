c29d519d90cfaa4c0fc06e51c36a828ddab31171
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-12-11 16:09:41 +0100

    [snpgenie] preparation with filtering

--- workflow/scripts/snpgenie_prep.py
+++ workflow/scripts/snpgenie_prep.py
@@ -1,32 +1,41 @@
 #!/usr/bin/env python3
-from fuc import pyvcf
+import vcf
 
+def f_add_DP_and_AF(record_INFO):
+    Rtot = record_INFO['Rtot']
+    Ftot = record_INFO['Ftot']
+    DP = int(Rtot) + int(Ftot)
 
-def add_vcf_DP_and_AF(vcf_in, vcf_out):
-    """
-    # alter vcf file such that it matches the necessary input of SNPGenie
-    # https://github.com/chasewnelson/SNPGenie#snpgenie-input
-    """
-    vf = pyvcf.VcfFrame.from_file(vcf_in)
+    Rvar = record_INFO['Rvar']
+    Fvar = record_INFO['Fvar']
+    AF = (int(Rvar) + int(Fvar)) / DP
 
-    Rtot = vf.df["INFO"].str.split("Rtot=").str[1].str.split(";").str[0]
-    Ftot = vf.df["INFO"].str.split("Ftot=").str[1].str.split(";").str[0]
-    DP = Rtot.astype("int") + Ftot.astype("int")
+    record_INFO["DP"]= DP
+    record_INFO["AF"]= AF
 
-    Rvar = vf.df["INFO"].str.split("Rvar=").str[1].str.split(";").str[0]
-    Fvar = vf.df["INFO"].str.split("Fvar=").str[1].str.split(";").str[0]
-    AF = (Rvar.astype("int") + Fvar.astype("int")) / DP
+    return record_INFO
 
-    vf.df["INFO"] = vf.df["INFO"] + ";DP=" + DP.astype("str")
-    vf.df["INFO"] = vf.df["INFO"] + ";AF=" + AF.astype("str")
+def f_windows_covering_positions(record_INFO):
+    return len([key for key in record.INFO.keys() if key.startswith("Freq")])
 
-    vf.to_file(vcf_out)
+def f_windows_pass_posterior(record_INFO):
+    posterior_threshold = 0.8
+    post_keys = [key for key in record.INFO.keys() if key.startswith("Post")]
+    post_values = [record_INFO[key][0] for key in post_keys]
+
+    return len([0 for x in post_values if float(x)>=posterior_threshold])
 
 
 def main(fname_snvs_vcf_in, fname_snvs_vcf_out):
+    vcf_reader = vcf.Reader(open(fname_snvs_vcf_in, 'r'))
+    vcf_writer = vcf.Writer(open(fname_snvs_vcf_out, 'w'), vcf_reader)
 
-    # add DP4 to vcf file
-    add_vcf_DP_and_AF(fname_snvs_vcf_in, fname_snvs_vcf_out)
+    for record in vcf_reader:
+        n_windows_covering_positions = f_windows_covering_positions(record.INFO)
+        n_windows_pass_posterior = f_windows_pass_posterior(record.INFO)
+        if (n_windows_covering_positions>1) & (n_windows_pass_posterior>1):
+            record.INFO = f_add_DP_and_AF(record.INFO)
+            vcf_writer.write_record(record)
 
 
 if __name__ == "__main__":
